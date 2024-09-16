d871596ad8fc36404f9b0b8153c4ef616f4e1605
mohkcotS <duylm2016@gmail.com>
2024-09-16 14:56:36 +0700

    Choose position to set up ship

--- src/Main/MouseHandler.java
+++ src/Main/MouseHandler.java
@@ -1,6 +1,8 @@
 package Main;
 
 
+import Battle.Coordinate;
+
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -8,6 +10,7 @@ import java.awt.event.MouseMotionListener;
 public class MouseHandler implements MouseListener, MouseMotionListener {
     GamePanel gp;
     int col,row,col1=-1,row1=-1;
+    int index; // shipSetup - 1
     int computerRow, computerCol;
     public MouseHandler(GamePanel gp){
         this.gp = gp;
@@ -19,7 +22,9 @@ public class MouseHandler implements MouseListener, MouseMotionListener {
     @Override
     public void mousePressed(MouseEvent e) {
         setUpBoardPlayer(e);
-        }
+        setUpShip();
+
+    }
 
 //        if(e.getX() >= 540 &&e.getX() <= 900 && e.getY() >= 96 && e.getY() <= 456 ){
 //        computerCol = (e.getX() - 540)/36;
@@ -57,15 +62,27 @@ public class MouseHandler implements MouseListener, MouseMotionListener {
         if(e.getX() >= 60 &&e.getX() <= 420 && e.getY() >= 96 && e.getY() <= 456 ){
             col = (e.getX() - 60)/36;
             row =(e.getY() - 96)/36;
-            System.out.println("Player: "+ (char)(col+65)+ row);
         }
         if(gp.b.getFromBoardPlayer(col,row) == 0 && e.getX() >= 60 &&e.getX() <= 420 && e.getY() >= 96 && e.getY() <= 456 ){
             gp.b.setUpBoardPlayer(col,row, -gp.b.shipSetUp);
+            gp.player.ship.get(index).addCoordinate(new Coordinate(col,row));
         }
         else if(gp.b.getFromBoardPlayer(col,row) == -gp.b.shipSetUp){
             gp.b.setUpBoardPlayer(col,row, 0);
+            gp.player.ship.get(index).removeCoordinate(new Coordinate(col,row));
         }
     }
+
+    public void setUpShip(){
+        gp.player.ship.get(index).sort();
+        gp.player.ship.get(index).setupStatusNo = gp.player.ship.get(index).checkValid();
+        gp.ui.statusNo = gp.player.ship.get(index).setupStatusNo;
+        if (gp.b.getFromBoardPlayer(col,row) != -gp.b.shipSetUp &&gp.b.getFromBoardPlayer(col,row) != 0) {
+            gp.ui.statusNo = 4;
+        }
+    }
+
+
 
     public void position(MouseEvent e){
         if(e.getX() >= 60 &&e.getX() <= 420 && e.getY() >= 96 && e.getY() <= 456 ){
--- src/Ship/Ship.java
+++ src/Ship/Ship.java
@@ -4,16 +4,64 @@ import Battle.Coordinate;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Ship {
     String name;
-    String status = "Alive";
+    String playingStatus = "Alive";
+    public int setupStatusNo=3;
     int size;
-    ArrayList<Coordinate> cor = new ArrayList<>();
+    public ArrayList<Coordinate> cor = new ArrayList<>();
     int health;
     Color c ;
 
     public void sort(){
+        cor.sort(Comparator.comparing(Coordinate::getX));
+        cor.sort(Comparator.comparing(Coordinate::getY));
+    }
 
+    public void print(){
+        System.out.println(cor.size());
+    }
+
+    public void addCoordinate(Coordinate c){
+        cor.add(c);
+    }
+
+    public void removeCoordinate(Coordinate c){
+       for(int i = 0; i<cor.size();i++){
+           if(cor.get(i).getX() == c.getX() && cor.get(i).getY() == c.getY()){
+               cor.remove(i);
+               break;
+           }
+       }
+    }
+
+    public int checkValid(){
+        int x = 0;
+        int y = 0;
+        if(cor.isEmpty() ){
+            return 3;
+        }
+        for (int i =1 ;i<cor.size();i++){
+            if(cor.get(i).getX() - cor.get(i-1).getX() > 1 || cor.get(i).getY() - cor.get(i-1).getY() > 1){
+                return 2;
+            }
+
+            if(cor.get(i).getX() - cor.get(i-1).getX() > 0){
+                x = 1;
+            }
+            if(cor.get(i).getY() - cor.get(i-1).getY() > 0){
+                y = 1;
+            }
+            if(x == 1 && y == 1){
+                return 2;
+            }
+        }
+        if(cor.size() != size){
+            return  1;
+        }
+        return 0;
     }
 }
